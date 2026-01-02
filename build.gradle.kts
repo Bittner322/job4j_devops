b64f9a724c49f96064f518800625fa1eaad2c17a
bittner322 <sandreyf1972@gmail.com>
2026-01-02 16:12:19 +0600

    Оптимизация и профилирование Gradle [#505354]

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,3 +48,23 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.register<Zip>("archiveResources") {
+    group = "custom optimization"
+    description = "Archives the resources folder into a ZIP file"
+
+    val inputDir = file("src/main/resources")
+    val outputDir = layout.buildDirectory.dir("archives")
+
+    inputs.dir(inputDir) // Входные данные для инкрементальной сборки
+    outputs.file(outputDir.map { it.file("resources.zip") }) // Выходной файл
+
+    from(inputDir)
+    destinationDirectory.set(outputDir)
+    archiveFileName.set("resources.zip")
+
+    doLast {
+        println("Resources archived successfully at ${outputDir.get().asFile.absolutePath}")
+    }
+}
+
